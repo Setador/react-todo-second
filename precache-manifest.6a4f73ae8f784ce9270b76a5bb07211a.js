639766ed48beaec10b31238b6a4dc0d1bb67b937
Gabriel Macias <Setador@users.noreply.github.com>
2020-01-09 01:57:08 -0800

    Updates

---
+++ precache-manifest.6a4f73ae8f784ce9270b76a5bb07211a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65cca7aaefe957a0b919805a43d0954",
+    "url": "/react-todo-second/index.html"
+  },
+  {
+    "revision": "a1910fe87d4fb69eb4fe",
+    "url": "/react-todo-second/static/css/main.4540588e.chunk.css"
+  },
+  {
+    "revision": "74687da1769a16d6b769",
+    "url": "/react-todo-second/static/js/2.ce992d1c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo-second/static/js/2.ce992d1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1910fe87d4fb69eb4fe",
+    "url": "/react-todo-second/static/js/main.3158bb5f.chunk.js"
+  },
+  {
+    "revision": "ad7f6a7d3d0e35e90d30",
+    "url": "/react-todo-second/static/js/runtime-main.4589e4e7.js"
+  }
+]);
